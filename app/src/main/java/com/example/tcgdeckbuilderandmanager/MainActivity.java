a30f68b8d3226afdb83b8da0ef72eb5aed65fcbf
Edward Leon <edleon761@gmail.com>
2023-11-29 16:13:43 -0800

    Updated files but I'll be restarting from the beginning

--- app/src/main/java/com/example/tcgdeckbuilderandmanager/MainActivity.java
+++ app/src/main/java/com/example/tcgdeckbuilderandmanager/MainActivity.java
@@ -9,14 +9,15 @@ import androidx.lifecycle.ViewModelProvider;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final int NEW_WORD_ACTIVITY_REQUEST_CODE = 1;
+    public static final int NEW_CARD_ACTIVITY_REQUEST_CODE = 1;
 
     private CardViewModel mCardViewModel;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerView = findViewById(R.id.recyclerview);
+
         final CardListAdapter adapter = new CardListAdapter(new CardListAdapter.CardDiff());
+
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
@@ -42,23 +45,21 @@ public class MainActivity extends AppCompatActivity {
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(view -> {
             Intent intent = new Intent(MainActivity.this, NewCardActivity.class);
-            startActivityForResult(intent, NEW_WORD_ACTIVITY_REQUEST_CODE);
+            startActivityForResult(intent, NEW_CARD_ACTIVITY_REQUEST_CODE);
         });
     }
 
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if (requestCode == NEW_WORD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
+        if (requestCode == NEW_CARD_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK) {
             Bundle bundle = new Bundle(data.getExtras());
 
-
-
-
-
             //mCardViewModel.insert(card);
+
         } else {
             Toast.makeText(
+
                     getApplicationContext(),
                     R.string.empty_not_saved,
                     Toast.LENGTH_LONG).show();
